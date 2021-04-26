491c286e502bf48fec6e9a0d540fbf6a38a66d4b
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-04-26 18:40:23 +0600

    add employees недоделал

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,21 @@ $('.header__slider').slick({
     dots:true
 });
 
+$('.employees__slider-top').slick({
+    arrows:false,
+    
+    asNavFor: '.employees__slider-sub',
+});
+
+$('.employees__slider-sub').slick({
+    slidesToShow:3,
+    slidesToScroll:1,
+    asNavFor: '.employees__slider-top',
+    centerMode: true, 
+    nextArrow:'<button class="employees__arrow employees__arrow-next"><img class="employees__arrow-img" src="images/employees-arrow-right.png" alt=""></button>',
+    prevArrow:'<button class="employees__arrow employees__arrow-prev"><img class="employees__arrow-img" src="images/employees-arrow-left.png" alt=""></button>',
+
+   
+});
   
 });
