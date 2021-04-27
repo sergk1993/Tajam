0505cc65a7a40602f749245c7607e270b37c2ed1
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-04-27 08:55:45 +0600

    add employees

--- app/js/main.js
+++ app/js/main.js
@@ -16,8 +16,8 @@ $('.employees__slider-sub').slick({
     slidesToScroll:1,
     asNavFor: '.employees__slider-top',
     centerMode: true, 
-    nextArrow:'<button class="employees__arrow employees__arrow-next"><img class="employees__arrow-img" src="images/employees-arrow-right.png" alt=""></button>',
-    prevArrow:'<button class="employees__arrow employees__arrow-prev"><img class="employees__arrow-img" src="images/employees-arrow-left.png" alt=""></button>',
+    nextArrow:'<button class="employees__arrow employees__arrow-next"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
+    prevArrow:'<button class="employees__arrow employees__arrow-prev"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17" stroke="white"/></svg></button>',
 
    
 });
