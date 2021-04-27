ba5f9d201b3966a1336ec2dcc32535fac868c8e3
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-04-27 13:03:41 +0600

    media header

--- app/js/main.js
+++ app/js/main.js
@@ -18,8 +18,10 @@ $('.employees__slider-sub').slick({
     centerMode: true, 
     nextArrow:'<button class="employees__arrow employees__arrow-next"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
     prevArrow:'<button class="employees__arrow employees__arrow-prev"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17" stroke="white"/></svg></button>',
-
-   
 });
-  
+
+$('.header__btn').on('click', function(){
+    $('.menu').toggleClass('menu--active')
+});
+
 });
